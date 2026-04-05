ff43ddc62db8595f67bd23d4aace0541d2c03897
rockboundgaming <rockboundgaming@gmail.com>
2026-04-05 21:24:43 -0230

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.add('active');
+                entry.target.classList.add('visible');
                 observer.unobserve(entry.target);
             }
         });
